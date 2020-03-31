de0d296ca6c51839f2e004526e18d3a6b7b5f7f7
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-03-31 16:56:17 -0500

    Created Matrix and Vector classes.

---
+++ MatrixandVector/src/main/java/org/kickstats/matrixandvector/Matrix.java
@@ -0,0 +1,12 @@
+
+package org.kickstats.matrixandvector;
+
+/**
+ * Instructions for creating and calculating matrices.
+ * 
+ * @author Chase Sonnemaker
+ * @version 31 March 2020
+ */
+public class Matrix {
+    
+}
---
+++ MatrixandVector/src/main/java/org/kickstats/matrixandvector/Vector.java
@@ -0,0 +1,12 @@
+
+package org.kickstats.matrixandvector;
+
+/**
+ * Instructions for creating and calculating vectors.
+ * 
+ * @author Chase Sonnemaker
+ * @version 31 March 2020
+ */
+public class Vector {
+    
+}
