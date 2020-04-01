2628c5429e253417aabd8bcb785c20a4e973b595
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-03-31 22:54:28 -0500

    Created stubs for the Matrix2D constructors.

--- MatricesAndVertices/src/main/java/org/kickstats/matricesandvectors/Matrix2D.java
+++ MatricesAndVertices/src/main/java/org/kickstats/matricesandvectors/Matrix2D.java
@@ -9,6 +9,10 @@ package org.kickstats.matricesandvectors;
  */
 public class Matrix2D {
     
+    public enum vectorType{
+        COLUMN, ROW
+    }// vectorType
+    
     /**
      * Number at row 0, column 0.
      */
@@ -49,4 +53,38 @@ public class Matrix2D {
      */
     Vector2D column1;
     
+    
+    /**
+     * Creates a 2x2 identity matrix instance.
+     */
+    public Matrix2D() {
+        
+    }// Matrix2D()
+    
+    
+    /**
+     * Creates a 2x2 matrix instance.
+     * 
+     * @param p00 Number at row 0, column 0.
+     * @param p01 Number at row 0, column 1.
+     * @param p10 Number at row 1, column 0.
+     * @param p11 Number at row 1, column 1.
+     */
+    public Matrix2D(double p00, double p01, double p10, double p11) {
+        
+    }// Matrix2D(double, double, double, double)
+    
+    
+    /**
+     * Creates a 2x2 matrix instance.
+     * 
+     * @param v0 First vector in the matrix (top or left-most).
+     * @param v1 Second vector in the matrix (bottom or right-most).
+     * @param type ROW if vectors are rows or COLUMN if vectors are columns.
+     */
+    public Matrix2D(Vector2D v0, Vector2D v1, vectorType type) {
+        
+    }// Matrix(Vector2D, Vector2D, vectorType)
+    
+            
 }// Matrix2D
