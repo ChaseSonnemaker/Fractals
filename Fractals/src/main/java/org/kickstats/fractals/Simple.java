441e3c5489a4177b43a74b1d36de92fea0588010
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-11 18:22:41 -0500

    feat: Add Simple class
    Simple is a class that creates a window on which images can be drawn. The code to make this was transcribed from Leon Tabak's code.

---
+++ Fractals/src/main/java/org/kickstats/fractals/Simple.java
@@ -0,0 +1,45 @@
+
+package org.kickstats.fractals;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+/**
+ * Create and display a window.
+ * 
+ * Draw something in the window by instantiating another class.
+ * 
+ * @author Chase Sonnemaker transcribed from Leon Tabak's code
+ * @version 11 April 2020
+ */
+public class Simple extends JFrame {
+    
+    private static final int SIMPLE_WIDTH = 512;
+    private static final int SIMPLE_HEIGHT = 512;
+    private static final String SIMPLE_TITLE = "Simple";
+    
+    
+    public Simple() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(SIMPLE_WIDTH, SIMPLE_HEIGHT);
+        this.setTitle(SIMPLE_TITLE);
+        
+        Container pane = this.getContentPane();
+        
+        this.setVisible(true);
+    }// Simple()
+    
+    /**
+     * Creates an instance of this class. 
+     */
+    public static void main(String[] args) {
+        Simple simple = new Simple();
+    }// main(String[])
+    
+    
+}// Simple
