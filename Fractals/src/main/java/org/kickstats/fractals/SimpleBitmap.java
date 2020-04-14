38c48ee5f05dacd6374d199b04a3623971e943ed
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-14 10:23:52 -0500

    docs: Update SimpleBitmap Javadocs
    Mostly added more information about how the functions are used and what they do to create the image. Also added a few lists of steps so people could potentially use methods of the class in the future and spent time defining the class and image outcome.

--- Fractals/src/main/java/org/kickstats/fractals/SimpleBitmap.java
+++ Fractals/src/main/java/org/kickstats/fractals/SimpleBitmap.java
@@ -9,13 +9,16 @@ import java.awt.image.WritableRaster;
 import javax.swing.JPanel;
 
 /**
- * Creates a panel and draws an image.
+ * Creates a panel and draws a piece of the Mandelbrot Set.
  * 
- * Currently creates an image of a screen where everything above a diagonal is
- * yellow and everything below the diagonal is black.
+ * Uses class constants to map each pixel of the panel onto a 
+ * point in the complex plane, the determines if that point is 
+ * a member of the Mandelbrot Set and colors the pixel accordingly.
+ * The complex plane being mapped to has a real range of -1 to -0.8
+ * and an imaginary range of 0.22 to 0.35.
  * 
  * @author Chase Sonnemaker transcribed from Leon Tabak's code
- * @version 11 April 2020
+ * @version 13 April 2020
  */
 public class SimpleBitmap extends JPanel {
     
@@ -42,8 +45,24 @@ public class SimpleBitmap extends JPanel {
     
     
     /**
-     * Maps a pair of real plane coordinates to coordinates in the 
-     * complex plane.
+     * Maps a pair of coordinates in one plane system (real) to coordinates 
+     * in another plane system (complex).
+     * 
+     * The starting plane is assumed to be a real plane with dimensions 
+     * defined by class constants and the resulting coordinates are assumed to 
+     * by a complex plane with dimensions defined by class constants. As such 
+     * the returned coordinate takes the form of a complex number. The 
+     * mapping occurs in three steps.
+     * <ul>
+     * <li> A translation of the points by subtracting the smallest x and y 
+     * value in the real plane from their respective coordinates.
+     * <li> A scaling of the points by taking the range of x and y values in 
+     * the complex plane and the range of x and y values in the real plane
+     * and multiplying them together, then multiplying them by their
+     * respective coordinates.
+     * <li> A translation of the points by adding the smallest x and y value of 
+     * the complex plane to their respective coordinates.
+     * </ul>
      * 
      * @param x An x coordinate in the real plane.
      * @param y A y coordinate in the real plane.
@@ -51,14 +70,14 @@ public class SimpleBitmap extends JPanel {
      * mapped to coordinates in the complex plane.
      */
     public Complex map(double x, double y) {
-        //u mapping
+        //u (x) mapping
         double uDist = MAX_U - MIN_U;
         double xRange = MAX_X - MIN_X;
         double xDist = x - MIN_X;
         
         double newU = MIN_U + uDist * (xDist / xRange);
         
-        //v mapping
+        //v (y) mapping
         double vDist = MAX_V - MIN_V;
         double yRange = MAX_Y - MIN_Y;
         double yDist = y - MIN_Y;
@@ -72,12 +91,19 @@ public class SimpleBitmap extends JPanel {
     
     
     /**
-     * Checks to see if a complex point is in the Mandelbrot Set by iterating.
+     * Checks to see if a complex point is in the Mandelbrot Set 
+     * through iteration.
      * 
-     * Stops if the number is not in the set or if a threshold of iterations 
-     * is reached. 
+     * Using the following formula...
+     * <P>
+     * z = z&sup2 + c
+     * <P>
+     * where z starts as the complex number point 0 + i * 0 and c is the 
+     * complex number point to test, this loop is repeated until the magnitude 
+     * of z is larger than 2, indicating the number is not in the Mandelbrot 
+     * set, or until a class defined number of iterations is reached. 
      * 
-     * @param c A complex number represent a point on the complex plane.
+     * @param c A complex number represent a point in the complex plane.
      * @return The number of iterations that occurred while testing to see 
      * if the point is in the Mandelbrot Set. 
      */
@@ -93,11 +119,27 @@ public class SimpleBitmap extends JPanel {
     
     
     /**
-     * Returns an array of integers representing rgb color values.
+     * Creates an array of integers representing an RGB color based on a 
+     * specified integer value.
      * 
-     * The returned color is based on the value of an integer and
-     * is designed to be used with the return value of the
-     * identifySet method.
+     * This method is designed to be used with the return value of the
+     * identifyMandelbrotMembership method and can be used to color an image
+     * of the Mandelbrot Set. This particular coloring scheme was designed to 
+     * color a piece of the Mandelbrot Set that looked like lightning, and 
+     * would likely be less useful for other pieces.
+     * <P>
+     * The internal workings of this method can be thought of like a piecewise 
+     * function which creates a series of color gradients.
+     * <ul>
+     * <li> For less than 10 iterations, the gradient is a series of 
+     * brightening greens.
+     * <li> For 10 to 24 iterations, the gradient is a series of 
+     * brightening blues.
+     * <li> For 25 to 74 iterations, the gradient is a series of 
+     * brightening yellows.
+     * <li> For more than 75 iterations, the gradient moves from a white to a 
+     * dark gray.
+     * </ul>
      * 
      * @param n An integer used to define the returned color.
      * @return An array of integers representing a rgb color value.
@@ -131,9 +173,9 @@ public class SimpleBitmap extends JPanel {
         }// else if
         
         else {
-            int r = 255 - 2 * n;
-            int g = 255 - 2 * n;
-            int b = 255 - 2 * n;
+            int r = 325 - 2 * n;
+            int g = 325 - 2 * n;
+            int b = 325 - 2 * n;
             
             int[] rgb = {r, g, b};
             return rgb;
@@ -143,7 +185,8 @@ public class SimpleBitmap extends JPanel {
     
     
     /**
-     * Creates an instance of this class.
+     * Creates an instance of this class which creates an image of the
+     * Mandelbrot Set with a class constant specified height and width.
      */
     public SimpleBitmap() {
         int w = BITMAP_WIDTH;
@@ -154,11 +197,14 @@ public class SimpleBitmap extends JPanel {
     
     
     /**
-     * Creates an image of a yellow and black screen where a diagonal splits
-     * the colors.
+     * Creates the Mandelbrot Set image by assigning each pixel a color based 
+     * on proximity to Mandelbrot Set membership.
      * 
-     * This image is drawn by manipulating the screen's pixels in a certain 
-     * pattern.
+     * To create the image, each pixel in the panel is mapped to a point on 
+     * a class constant defined complex plane. This point is then tested for 
+     * membership in the Mandelbrot Set. The number of completed iterations
+     * of the testing function determines the color of the pixel to create the 
+     * Mandelbrot Set image which is then transformed to properly fit the panel.
      * 
      * @param g A graphics object.
      */
