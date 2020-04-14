e519e5cd6aed3b31189521a64aa3517ae5e3b1d2
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-13 22:30:50 -0500

    docs: Update Simple Javadocs
    Added some non-Javadoc notes, detailed what the image draws, and gave a more detailed explanation as to how the class works.

--- Fractals/src/main/java/org/kickstats/fractals/Simple.java
+++ Fractals/src/main/java/org/kickstats/fractals/Simple.java
@@ -1,27 +1,32 @@
 
 package org.kickstats.fractals;
 
-
 import java.awt.Container;
 import javax.swing.JFrame;
 
-
 /**
- * Makes a window used to draw something.
+ * Creates a window used to draw a piece of the Mandelbrot Set.
+ * 
+ * The image is stylized using color manipulation 
+ * to look like a lightning storm.
  * 
  * @author Chase Sonnemaker transcribed from Leon Tabak's code
- * @version 11 April 2020
+ * @version 13 April 2020
  */
 public class Simple extends JFrame {
     
+    //Window Information
     private static final int SIMPLE_WIDTH = 512;
     private static final int SIMPLE_HEIGHT = 512;
     private static final String SIMPLE_TITLE = "Simple";
     
+    
     /**
-     * Creates and displays a window.
+     * Creates an instance of this class which creates a window.
      *
-     * Draw something in the window by instantiating another class.
+     * The window has a size and name defined in this class and draws 
+     * a piece of the Mandelbrot Set by creating an instance of the
+     * SimpleBitmap class.
      * 
      */
     public Simple() {
@@ -37,8 +42,10 @@ public class Simple extends JFrame {
         this.setVisible(true);
     }// Simple()
     
+    
     /**
-     * Creates an instance of this class. 
+     * Calls the constructor of this class to create a window
+     * with a Mandelbrot Set piece drawn on it.
      */
     public static void main(String[] args) {
         Simple simple = new Simple();
